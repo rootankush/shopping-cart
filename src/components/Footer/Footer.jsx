dd286c73c2c0eea0d93a227895073534d703b09d
rootankush <rootankushprompt@gmail.com>
2026-09-03 08:52:04 +0530

    feat: changed from Footer.css to Footer.module.css

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../Footer/Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <h5 className={styles.h5}>made by rootankush</h5>
+    </footer>
+  );
+}
+
+export default Footer;
