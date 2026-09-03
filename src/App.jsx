75c0323c88aede1485a4ca3350ce038d622270fb
rootankush <rootankushprompt@gmail.com>
2026-09-03 08:47:25 +0530

    feat: added React Router for switch between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Cartpage from "./pages/Cartpage/Cartpage";
+import Homepage from "./pages/Homepage/Homepage";
+import Shoppage from "./pages/Shoppage/Shoppage";
 
 function App() {
-  return <></>;
+  return (
+    <div className="site-wrapper">
+      <Header />
+      <main className="main-container">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<Shoppage />} />
+          <Route path="/cart" element={<Cartpage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
