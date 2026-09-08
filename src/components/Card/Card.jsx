8506ee4b131ea2ff9676e083cb16f1508a5a1ff9
rootankush <rootankushprompt@gmail.com>
2026-09-08 16:54:25 +0530

    style: made changes to Card Component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Card/Card.module.css";
 
-function Card({ title, price, image, category }) {
+function Card({ item, handleQuantity }) {
   return (
     <div className={styles.card}>
-      <img className={styles.img} src={image} alt={title} />
-      <h5>{title}</h5>
-      <h5>${price}</h5>
-      <h5>{category}</h5>
-      <h5>Quantity:</h5>
-      <input type="number" />
-      <button type="button">Add to Cart</button>
+      <div className={styles.imgContainer}>
+        <img className={styles.img} src={item.image} alt={item.title} />
+      </div>
+      <div className={styles.details}>
+        <h4 className={styles.title}>{item.title}</h4>
+        <p className={styles.price}>${item.price.toFixed(2)}</p>
+        <span className={styles.category}>{item.category}</span>
+      </div>
+
+      <div className={styles.actionContainer}>
+        <div className={styles.quantityWrapper}>
+          <label htmlFor={`qty-${item.id}`}>Qty:</label>
+          <input
+            id={`qty-${item.id}`}
+            className={styles.input}
+            type="number"
+            min="0"
+          />
+        </div>
+        <button className={styles.button} type="button">
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 }
