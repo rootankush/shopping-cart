088f0d7ac026f6a9fe5f7b9603fdff093b2321c6
rootankush <rootankushprompt@gmail.com>
2026-09-11 12:26:37 +0530

    style: changed the color paleete

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import styles from "../Card/Card.module.css";
 
-function Card({ item, handleQuantity }) {
+function Card({ item, handleIncrement }) {
+  const [localQuantity, setLocalQuantity] = useState(0);
+
+  const sendData = () => {
+    handleIncrement(item, localQuantity);
+  };
+
   return (
-    <div className={styles.card}>
+    <form className={styles.card}>
       <div className={styles.imgContainer}>
         <img className={styles.img} src={item.image} alt={item.title} />
       </div>
       <div className={styles.details}>
         <h4 className={styles.title}>{item.title}</h4>
-        <p className={styles.price}>${item.price.toFixed(2)}</p>
+        <p className={styles.price}>${item.price}</p>
         <span className={styles.category}>{item.category}</span>
       </div>
 
@@ -20,14 +26,14 @@ function Card({ item, handleQuantity }) {
             id={`qty-${item.id}`}
             className={styles.input}
             type="number"
-            min="0"
+            onChange={(e) => setLocalQuantity(e.target.value)}
           />
         </div>
-        <button className={styles.button} type="button">
+        <button className={styles.button} type="button" onClick={sendData}>
           Add to Cart
         </button>
       </div>
-    </div>
+    </form>
   );
 }
 
